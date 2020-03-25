2e534489f21aa4d81ccc9479d7fe98224377a1f1
Jan <jansykutera@gmail.com>
2020-03-25 22:23:31 +0100

    created db

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Players(models.Model):
+    player = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.player
