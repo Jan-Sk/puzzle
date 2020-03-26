621c50006ae3169a2c44e9ca1c0680a0b5790063
Jan <jansykutera@gmail.com>
2020-03-26 20:17:21 +0100

    add url

--- testapp/urls.py
+++ testapp/urls.py
@@ -6,6 +6,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
 path('', views.home, name='testapp-home'),
 path('upload/', views.upload, name='testapp-upload'),
-
+path('register/', views.upload, name='testapp-register'),
+path('login/', views.upload, name='testapp-login'),
 
 ]
