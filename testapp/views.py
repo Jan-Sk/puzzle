564d88a6e72b558e136aba89e48e16c21680ec0c
Jan <jansykutera@gmail.com>
2020-03-28 15:08:17 +0100

    commit

--- testapp/views.py
+++ testapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from testapp.models import Players
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,20 @@ def register(request):
 	return render(request, 'testapp/register.html')
 
 def login(request):
+	if request.method == 'POST':
+	        username = request.POST['username']
+	        password =  request.POST['password']
+	        post = Players.objects.filter(player=username)
+
+
+	        if post:
+	        	request.session['username'] = username
+	        	print('ustawiona sesja')
+	        	return redirect("testapp-home")
+	        else:
+	        	print('nieustawiona sesja')
+	        	return render(request, 'testapp/login.html')
+
 	return render(request, 'testapp/login.html')
 
 def upload(request):
