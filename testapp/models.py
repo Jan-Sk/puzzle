76134875f912de19df0076288d443ac3c88b279e
Jan <jansykutera@gmail.com>
2020-04-25 22:36:32 +0200

    created log system

--- testapp/models.py
+++ testapp/models.py
@@ -8,3 +8,12 @@ class Players(models.Model):
 
     def __str__(self):
         return self.username
+
+class Logi(models.Model):
+    username = models.CharField(max_length=100)
+    action = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from testapp.models import Players
+from testapp.models import Players, Logi
 from django.contrib import messages
 
 # Create your views here.
@@ -19,7 +19,9 @@ def register(request):
 				return render(request, 'testapp/register.html')
 			else:
 				Players(username = username, password = password).save()
+				Logi(username = username, action = "acc created").save()
 				messages.success(request, 'acc created')
+
 				return redirect("testapp-login")
 	return render(request, 'testapp/register.html')
 
@@ -35,10 +37,13 @@ def login(request):
 			if post:
 				if post.first().password == password:
 					request.session['username'] = username
+					print(username)
+					Logi(username = username, action = "logged in").save()
 					print('ustawiona sesja')
 					messages.success(request, 'logged in')
 					return redirect("testapp-home")
 				else:
+					Logi(username = username, action = "wrong pass").save()
 					messages.warning(request, 'wrong pass')
 					return render(request, 'testapp/login.html')
 			else:
@@ -49,6 +54,9 @@ def login(request):
 
 def logout(request):
 	try:
+		username = request.session['username']
+		Logi(username = username, action = "logged out").save()
+
 		del request.session['username']
 		messages.success(request, 'logged out')
 	except:
@@ -60,6 +68,8 @@ def upload(request):
 
 def testarea(request):
 	if request.session.has_key('username'):
+		username = request.session['username']
+		Logi(username = username, action = "accessed restricted area").save()
 		messages.success(request, 'welcome in restricted area!')
 		return render(request, 'testapp/testarea.html')
 	else:
